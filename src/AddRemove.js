869b3a5ac03e56b7e676d8423271e96b759f6cfa
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-02-19 22:05:02 +0430

    Disabling unused variables

--- src/AddRemove.js
+++ src/AddRemove.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-restricted-globals */
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-restricted-globals */
 import _ from 'lodash';
 import {
   itemsArr, tasksListShow, getItems, saveLocalStorage,
@@ -16,6 +16,7 @@ const updateItem = function (currentItemIndex, value) {
   const newItem = itemsArr[currentItemIndex];
   newItem.description = value;
   itemsArr.splice(currentItemIndex, 1, newItem);
+  // eslint-disable-next-line no-use-before-define
   saveLocalStorage(itemsArr);
 };
 
